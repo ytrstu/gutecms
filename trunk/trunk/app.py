bdae69ddb8b02294e328ddfffc92e387dcf67ac9
David.J.Noble <David.J.Noble@b3d6f863-d454-0410-aa62-3f6a3cb5f8d1>
2008-08-23 20:39:52 +0000

    pulled from github repository

--- trunk/trunk/app.py
+++ trunk/trunk/app.py
@@ -1,4 +1,20 @@
-import logging
+#
+# Copyright 2008 David Noble <dnoble@dnoble.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging, traceback
 import os
 import re
 import cgi
@@ -19,8 +35,27 @@ class Page(db.Model):
   content = db.TextProperty()
   date = db.DateTimeProperty(auto_now_add=True)
   author = db.UserProperty()
+  def formatted_date(self):
+    return self.date.strftime('%d %b %Y %H:%M:%S')
 
-class BaseRequestHandler(webapp.RequestHandler):
+class PageRenderer(webapp.RequestHandler):
+  def get(self, url):
+    try:
+      page = Page.all().filter('url', url).get()
+      if page:
+        path = os.path.join(os.path.dirname(__file__), 'html', 'render.html')
+        self.response.out.write(template.render(path, { 'page': page, }))
+      else:
+        self.error(404)
+        path = os.path.join(os.path.dirname(__file__), 'html', '404.html')
+        self.response.out.write(template.render(path, { }))
+    except:
+      logging.error(traceback.format_exc())
+      self.error(500)
+      path = os.path.join(os.path.dirname(__file__), 'html', '500.html')
+      self.response.out.write(template.render(path, { }))
+
+class EditRequestHandler(webapp.RequestHandler):
   roles = None
 
   def respond(self, filename, payload):
@@ -38,12 +73,19 @@ class BaseRequestHandler(webapp.RequestHandler):
     payload['user'] = user
     payload['role'] = role
     payload['logout_url'] = users.create_logout_url(self.request.uri)
-    path = os.path.join(os.path.dirname(__file__), 'html', filename)
+    path = os.path.join(os.path.dirname(__file__), 'html', 'edit', filename)
+    logging.info(path)
     self.response.out.write(template.render(path, payload))
 
   def not_found(self):
     self.error(404)
-    path = os.path.join(os.path.dirname(__file__), 'html', '404.html')
+    path = os.path.join(os.path.dirname(__file__), 'html', 'edit', '404.html')
+    self.response.out.write(template.render(path, { }))
+
+  def fail(self):
+    logging.error(traceback.format_exc())
+    self.error(500)
+    path = os.path.join(os.path.dirname(__file__), 'html', 'edit', '500.html')
     self.response.out.write(template.render(path, { }))
 
   def require_login(self):
@@ -71,41 +113,47 @@ class BaseRequestHandler(webapp.RequestHandler):
 
   def require_role(self, role):
     user = users.get_current_user()
-    if not self.has_role(user, 'Manager'):
+    if not self.has_role(user, role):
       self.respond('unauthorized.html', { })
       return False
     return True
 
-class RoleAssignmentEditor(BaseRequestHandler):
+class RoleAssignmentEditor(EditRequestHandler):
   def get(self, action):
-    if not self.require_login():
-      return
-    if not self.require_role('Manager'):
-      return
-    if action == 'list':
-      self.show_list()
-    elif action == 'add':
-      self.show_add_form()
-    elif action == 'modify':
-      self.show_modify_form()
-    elif action == 'delete':
-      self.show_delete_form()
-    else:
-      logging.warn('Unrecognized request action: %s' % action)
-      self.not_found()
+    try:
+      if not self.require_login():
+        return
+      if not self.require_role('Manager'):
+        return
+      if action == 'list':
+        self.show_list()
+      elif action == 'add':
+        self.show_add_form()
+      elif action == 'modify':
+        self.show_modify_form()
+      elif action == 'delete':
+        self.show_delete_form()
+      else:
+        logging.warn('Unrecognized request action: %s' % action)
+        self.not_found()
+    except:
+      self.fail()
 
   def post(self, action):
-    if not self.require_login():
-      return
-    if action == 'add':
-      self.apply_add_form()
-    elif action == 'modify':
-      self.apply_modify_form()
-    elif action == 'delete':
-      self.apply_delete_form()
-    else:
-      logging.warn('Unrecognized request action: %s' % action)
-      self.not_found()
+    try:
+      if not self.require_login():
+        return
+      if action == 'add':
+        self.apply_add_form()
+      elif action == 'modify':
+        self.apply_modify_form()
+      elif action == 'delete':
+        self.apply_delete_form()
+      else:
+        logging.warn('Unrecognized request action: %s' % action)
+        self.not_found()
+    except:
+      self.fail()
 
   def show_list(self):
     list = db.GqlQuery("SELECT * FROM RoleAssignment ORDER BY user ASC LIMIT 20")
@@ -125,7 +173,7 @@ class RoleAssignmentEditor(BaseRequestHandler):
     # TODO: verify that url is unique & has only valid characters
     role_assignment = RoleAssignment(user=user, role=role)
     key = role_assignment.put()
-    self.redirect('/_cms/roles/list')
+    self.redirect('/edit/roles/list')
 
   def show_modify_form(self):
     key = self.request.get('key')
@@ -153,7 +201,7 @@ class RoleAssignmentEditor(BaseRequestHandler):
     role_assignment.role = role
     role_assignment.content = self.request.get('content')
     role_assignment.put()
-    self.redirect('/_cms/roles/list')
+    self.redirect('/edit/roles/list')
 
   def show_delete_form(self):
     key = self.request.get('key')
@@ -172,38 +220,44 @@ class RoleAssignmentEditor(BaseRequestHandler):
     if not role_assignment:
       return self.not_found()
     role_assignment.delete()
-    self.redirect('/_cms/roles/list')
+    self.redirect('/edit/roles/list')
 
-class PageEditor(BaseRequestHandler):
+class PageEditor(EditRequestHandler):
   def get(self, action):
-    if not self.require_login():
-      return
-    if not self.require_role('Editor'):
-      return
-    if action == 'list':
-      self.show_list()
-    elif action == 'add':
-      self.show_add_form()
-    elif action == 'modify':
-      self.show_modify_form()
-    elif action == 'delete':
-      self.show_delete_form()
-    else:
-      logging.warn('Unrecognized request action: %s' % action)
-      self.not_found()
+    try:
+      if not self.require_login():
+        return
+      if not self.require_role('Editor'):
+        return
+      if action == 'list':
+        self.show_list()
+      elif action == 'add':
+        self.show_add_form()
+      elif action == 'modify':
+        self.show_modify_form()
+      elif action == 'delete':
+        self.show_delete_form()
+      else:
+        logging.warn('Unrecognized request action: %s' % action)
+        self.not_found()
+    except:
+      self.fail()
 
   def post(self, action):
-    if not self.require_login():
-      return
-    if action == 'add':
-      self.apply_add_form()
-    elif action == 'modify':
-      self.apply_modify_form()
-    elif action == 'delete':
-      self.apply_delete_form()
-    else:
-      logging.warn('Unrecognized request action: %s' % action)
-      self.not_found()
+    try:
+      if not self.require_login():
+        return
+      if action == 'add':
+        self.apply_add_form()
+      elif action == 'modify':
+        self.apply_modify_form()
+      elif action == 'delete':
+        self.apply_delete_form()
+      else:
+        logging.warn('Unrecognized request action: %s' % action)
+        self.not_found()
+    except:
+      self.fail()
 
   def show_list(self):
     list = db.GqlQuery("SELECT * FROM Page ORDER BY url ASC LIMIT 20")
@@ -224,7 +278,7 @@ class PageEditor(BaseRequestHandler):
     # TODO: verify that url is unique & has only valid characters
     page = Page(url=url, title=title, content=content, author=user)
     key = page.put()
-    self.redirect('/_cms/pages/list')
+    self.redirect('/edit/pages/list')
 
   def show_modify_form(self):
     logging.error('OK')
@@ -250,7 +304,7 @@ class PageEditor(BaseRequestHandler):
     page.title = self.request.get('title')
     page.content = self.request.get('content')
     page.put()
-    self.redirect('/_cms/pages/list')
+    self.redirect('/edit/pages/list')
 
   def show_delete_form(self):
     key = self.request.get('key')
@@ -269,25 +323,29 @@ class PageEditor(BaseRequestHandler):
     if not page:
       return self.not_found()
     page.delete()
-    self.redirect('/_cms/pages/list')
+    self.redirect('/edit/pages/list')
 
-class EditorConsole(BaseRequestHandler):
+class EditorConsole(EditRequestHandler):
   def get(self):
-    if not self.require_login():
-      return
-    self.respond('_cms.html', { })
-
-class PageRenderer(BaseRequestHandler):
-  def get(self, url):
-    page = Page.all().filter('url', url).get()
-    if not page:
-      return self.not_found()
-    self.respond('render.html', { 'page': page, })
+    try:
+      if not self.require_login():
+        return
+      user = users.get_current_user()
+      if self.has_role(user, 'Editor'):
+        self.redirect('/edit/roles/list')
+      elif self.has_role(user, 'Manager'):
+        self.redirect('/edit/roles/list')
+      elif self.has_role(user, 'Admin'):
+        self.redirect('/_ah/admin')
+      else:
+        self.respond('unauthorized.html', { })
+    except:
+      self.fail()
 
 application = webapp.WSGIApplication([
-                ('/_cms/roles/(.*)', RoleAssignmentEditor),
-                ('/_cms/pages/(.*)', PageEditor),
-                ('/_cms/?', EditorConsole),
+                ('/edit/roles/(.*)', RoleAssignmentEditor),
+                ('/edit/pages/(.*)', PageEditor),
+                ('/edit/?', EditorConsole),
                 ('(/.*)', PageRenderer),
               ])
 def main():
